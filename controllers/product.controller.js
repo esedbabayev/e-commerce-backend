a23877e80cac3059faf20e9ec74f3ac6a309a148
Asad Babayev <esedbabayev12@gmail.com>
2024-09-06 02:43:22 +0400

    complete controllers

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,4 +1,5 @@
 import { Product } from "../models/product.model.js";
+
 export const getAllProducts = async (request, response) => {
   try {
     const products = await Product.find();
@@ -10,7 +11,12 @@ export const getAllProducts = async (request, response) => {
 export const getProduct = async (request, response) => {
   const { id } = request.params;
   try {
-    const product = Product.findById(id);
+    const product = await Product.findById(id);
+
+    if (!product) {
+      return response.status(404).json({ message: "Product not found" });
+    }
+
     response.status(200).send({ product });
   } catch (error) {
     response.status(500).send({ error });
@@ -20,11 +26,68 @@ export const deleteProduct = async (request, response) => {
   const { id } = request.params;
 
   try {
-    const deletedProduct = Product.findByIdAndDelete(id);
+    const deletedProduct = await Product.findByIdAndDelete(id);
+
+    if (!deletedProduct) {
+      return response.status(404).send({ message: "Product not found" });
+    }
+
     response.status(200).send({ deletedProduct });
   } catch (error) {
     response.status(500).send({ error });
   }
 };
-export const addProduct = async (request, response) => {};
-export const updateProduct = async (request, response) => {};
+export const addProduct = async (request, response) => {
+  const {
+    title,
+    description,
+    coverImage,
+    price,
+    size,
+    color,
+    rating,
+    quantity,
+    discount,
+  } = request.body;
+
+  if (!title || !description || !coverImage || !price || !quantity) {
+    return response.status(400).send({ message: "Missing required fields" });
+  }
+
+  const newProduct = {
+    title,
+    description,
+    coverImage,
+    price,
+    size,
+    color,
+    rating,
+    quantity,
+    discount,
+  };
+
+  try {
+    const createdProduct = await Product.create(newProduct);
+
+    response.status(201).send({ createdProduct });
+  } catch (error) {
+    response.status(500).send(error.message);
+  }
+};
+export const updateProduct = async (request, response) => {
+  const { id } = request.params;
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, request.body, {
+      new: true,
+    });
+
+    if (!updatedProduct) {
+      return response.status(404).send({ message: "Product not found" });
+    }
+
+    response.status(200).send({ updatedProduct });
+  } catch (error) {
+    response.status(500).send(error);
+  }
+};
