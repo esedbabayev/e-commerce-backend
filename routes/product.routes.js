4c5b54e6be44c2697a8bbebb9a4dde4a6a81a98e
Asad Babayev <esedbabayev12@gmail.com>
2024-09-06 02:43:33 +0400

    import controllers

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,11 +1,20 @@
 import express from "express";
 
+// Controllers
+import {
+  getAllProducts,
+  getProduct,
+  deleteProduct,
+  addProduct,
+  updateProduct,
+} from "../controllers/product.controller.js";
+
 const router = express.Router();
 
-router.get("/", () => {});
-router.get("/:id", () => {});
-router.get("/:id", () => {});
-router.get("/:id", () => {});
-router.get("/:id", () => {});
+router.get("/", getAllProducts);
+router.get("/:id", getProduct);
+router.delete("/:id", deleteProduct);
+router.post("/", addProduct);
+router.patch("/:id", updateProduct);
 
 export default router;
