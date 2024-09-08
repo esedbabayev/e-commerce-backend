f4171da557108aee9b9455e648ead53576df2bd2
Asad Babayev <esedbabayev12@gmail.com>
2024-09-09 02:20:45 +0400

    minor changes

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from 'cors';
+import cors from "cors";
 import connectToDatabase from "./config/connect.js";
 
 // Routes
-import UserRoute from "./routes/user.routes.js"
+import UserRoute from "./routes/user.routes.js";
 import ProductRouter from "./routes/product.routes.js";
 
 const server = express();
 
 // Enable CORS for all origins
 server.use(cors());
-
 
 dotenv.config();
 server.use(express.json());
@@ -21,9 +20,8 @@ server.use(cookieParser());
 const PORT = process.env.PORT;
 
 // Routes
-server.use("/api/users", UserRoute)
+server.use("/api/users", UserRoute);
 server.use("/api/products", ProductRouter);
-
 
 server.listen(PORT, () => {
   console.log(`Server is listening on PORT: ${PORT}`);
