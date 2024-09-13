f12aaff1675c90b3c1c6443e8a0032283b6e5cf5
asadbabayev <asadyb@code.edu.az>
2024-09-13 17:30:25 +0400

    create category model

---
+++ models/category.model.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const CategorySchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+});
+
+export const Category = mongoose.model("category", CategorySchema);
