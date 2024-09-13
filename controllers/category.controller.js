117443e3483e1e4e4ed88571dc5305f46dcb7f3a
asadbabayev <asadyb@code.edu.az>
2024-09-13 17:29:26 +0400

    create category controller

---
+++ controllers/category.controller.js
@@ -0,0 +1,5 @@
+export const getAllCategories = () => {};
+export const getCategory = () => {};
+export const createCategory = () => {};
+export const deleteCategory = () => {};
+export const updateCategory = () => {};
