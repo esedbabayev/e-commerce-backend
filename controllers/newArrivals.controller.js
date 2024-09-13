a6206c2e480a3d4391824e34c99d580b5daa982d
asadbabayev <asadyb@code.edu.az>
2024-09-13 17:29:47 +0400

    add limit

--- controllers/newArrivals.controller.js
+++ controllers/newArrivals.controller.js
@@ -1,8 +1,10 @@
 import { Product } from "../models/product.model.js";
 
 export const getNewArrivals = async (request, response) => {
+  const { limit } = request.query;
+
   try {
-    const products = await Product.find({ newArrival: true });
+    const products = await Product.find({ newArrival: true }).limit(limit);
     response.status(200).send({ products });
   } catch (error) {
     response.status(500).send({ error });
