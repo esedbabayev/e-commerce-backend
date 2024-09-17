cf1fa68b0c18631e14f1e517559b55faeb18b87a
Ulkar Aghayeva <ulkarragh@code.edu.az>
2024-09-17 14:51:12 +0400

    save changes

--- controllers/newArrivals.controller.js
+++ controllers/newArrivals.controller.js
@@ -1,8 +1,9 @@
 import { Product } from "../models/product.model.js";
 
 export const getNewArrivals = async (request, response) => {
+  const {limit} = request.params
   try {
-    const products = await Product.find({ newArrival: true });
+    const products = await Product.find({ newArrival: true }).limit(limit);
     response.status(200).send({ products });
   } catch (error) {
     response.status(500).send({ error });
